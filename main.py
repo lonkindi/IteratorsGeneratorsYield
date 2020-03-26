babd2023469a7a62dd975f442c24e9390a00206a
lonkindi <legions.ru@gmail.com>
2020-03-26 14:22:46 +0500

    Task 1

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import requests
 import json
-from pprint import pprint
 
 
 class CountriesInWiki:
     def __init__(self, path):
-        self.dict_countries = dict()
         self.list_countries = list()
         self.path = path
         self.session = requests.session()
@@ -13,28 +11,33 @@ class CountriesInWiki:
         with open(path, encoding='utf-8') as file:
             self.j_file = json.load(file)
         for item in self.j_file:
-            country_rus = item['translations']['rus']['common']
-            country_link = country_rus.replace(' ', '_')
-            wiki_link = f"https://ru.wikipedia.org/wiki/{country_link}"
-            self.dict_countries[item['translations']['rus']['common']] = wiki_link
-            self.list_countries.append({item['translations']['rus']['common']: wiki_link})
+            self.list_countries.append(item['translations']['rus']['common'])
 
     def __iter__(self):
+        f = open('output.txt', 'w')
+        f.close()
         return self
 
     def __next__(self):
-        self.counter += 1
-        if len(self.dict_countries) == self.counter-1:
-            self.counter = 0
-            raise StopIteration
-        return self.list_countries[self.counter - 1]
+        wrap = '\n'
+        with open('output.txt', 'a', encoding='utf-8') as out_file:
+            self.counter += 1
+            if len(self.list_countries) == self.counter-1:
+                print('Ссылки на wiki по этим странам сформированы и выгружены в файл <output.txt>')
+                self.counter = 0
+                raise StopIteration
+            item = self.list_countries[self.counter - 1]
+            if self.counter == len(self.list_countries):
+                wrap = ''
+            country_link = item.replace(' ', '_')
+            wiki_link = f"{item} - https://ru.wikipedia.org/wiki/{country_link}{wrap}"
+            out_file.write(wiki_link)
+            return (f'№ {self.counter} - {item}')
 
 
 if __name__ == '__main__':
     j_path = 'countries.json'
-    # response = requests.get('https://ru.wikipedia.org/wiki/Афганистан')
-    # print(response.status_code)
     countries_in_wiki = CountriesInWiki(j_path)
-    print(len(countries_in_wiki.list_countries))
+    print(f'Стран в файле: {len(countries_in_wiki.list_countries)} (см. ниже)')
     for item in countries_in_wiki:
-        pprint(item)
+        print(item)
